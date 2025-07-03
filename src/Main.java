c0829d455a9074e0d038c9305e49cb748cd68706
Andrey Voronkov <voronkov071483@mail.ru>
2025-07-03 14:48:41 +0400

    test6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        System.out.println("Hello github!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
